41af7a1c076114462c6041cca1b11f038352a80d
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-07-29 07:17:26 +0500

    Change console.log to console.warn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './redux/store';
 
-// console.log(store);
+// console.warn(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
