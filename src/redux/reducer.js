71bfac4ee3176646f4d496bb19d5bbf6f89b676f
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-02 00:42:19 +0500

    return data

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,6 @@
 import { ADD_TO_CART, REMOVE_TO_CART } from './constant'
 
-export const cartData = (data = [], action) => {
+export const cartData = (data = [], action) => { //data = [] is initial value
     // if (action.type === ADD_TO_CART) {
     //     console.warn("Reducer Called", action);
     //     return action.data;
@@ -12,10 +12,10 @@ export const cartData = (data = [], action) => {
     switch (action.type) {
         case ADD_TO_CART:
             console.warn("ADD_TO_CART CONDITION", action);
-            return 1 + 1;
+            return [action.data, ...data];
 
         default:  //When No Case Matched
-            return "No Action Called";
+            return data;
     }
 
 
