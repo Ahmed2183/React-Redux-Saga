dfdb4bc7921b0ec59ddfb9d0dd3febb849805c87
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-01 23:35:21 +0500

    Replace if-else with switch statement

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,22 @@
-import { ADD_TO_CART } from './constant'
+import { ADD_TO_CART, REMOVE_TO_CART } from './constant'
 
 export const cartData = (data = [], action) => {
-    if (action.type === ADD_TO_CART) {
-        console.warn("Reducer Called", action);
-        return action.data;
+    // if (action.type === ADD_TO_CART) {
+    //     console.warn("Reducer Called", action);
+    //     return action.data;
+    // }
+    // else {
+    //     return "No Action Called";
+    // }
+
+    switch (action.type) {
+        case ADD_TO_CART:
+            console.warn("ADD_TO_CART CONDITION", action);
+            return 1 + 1;
+
+        default:  //When No Case Matched
+            return "No Action Called";
     }
-    else {
-        return "No Action Called";
-    }
+
+
 }
