0f958cbe8bb933ea40fd706d2d32a0e853dfef79
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-02 06:45:02 +0500

    Create Remove and Empty Cart caes

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART, REMOVE_TO_CART } from './constant'
+import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART } from './constant'
 
 export const cartData = (data = [], action) => { //data = [] is initial value
     // if (action.type === ADD_TO_CART) {
@@ -14,9 +14,17 @@ export const cartData = (data = [], action) => { //data = [] is initial value
             console.warn("ADD_TO_CART CONDITION", action);
             return [action.data, ...data];
 
+        case REMOVE_FROM_CART:
+            console.warn("REMOVE_FROM_CART CONDITION", action);
+            data.length = data.length ? data.length - 1 : [];
+            return [...data];
+
+        case EMPTY_CART:
+            console.warn("EMPTY_CART CONDITION", action);
+            data = [];
+            return [...data];
+
         default:  //When No Case Matched
             return data;
     }
-
-
 }
