60896d5bfade138862dedb5ad29b3d7fd0d313d9
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-02 06:44:20 +0500

    Create remove and empty cart actions

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,9 +1,25 @@
-import { ADD_TO_CART } from './constant'
+import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART } from './constant'
 
 export const addToCart = (data) => {
-    console.warn("Action Called", data);
+    console.warn("Action addToCart Called", data);
     return {
-        type: ADD_TO_CART ,
+        type: ADD_TO_CART,
         data
     }
 }
+export const removeFromCart = (data) => {
+    console.warn("Action removeFromCart Called", data);
+    return {
+        type: REMOVE_FROM_CART,
+        data
+    }
+}
+
+export const emptyCart = () => {
+    console.warn("Action removeFromCart Called");
+    return {
+        type: EMPTY_CART,
+    }
+}
+
+
