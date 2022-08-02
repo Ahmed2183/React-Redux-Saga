0349491493891a50049821eec3c9029570279ef3
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-02 06:43:57 +0500

    Create Remove and Empty Cart Button and use actions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux'
-import { addToCart } from '../redux/action';
+import { addToCart, emptyCart, removeFromCart } from '../redux/action';
 
 function Main() {
 
@@ -8,9 +8,15 @@ function Main() {
   const product = { name: 'Oppo', type: 'Mobile', price: '30000', color: 'black' }
 
   return (
+
     <div>
       <button onClick={() => dispatch(addToCart(product))}>Add To Cart</button>
+      <div>
+        <button onClick={() => dispatch(removeFromCart(product.name))}>Remove From Cart</button>
+        <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
+      </div>
     </div>
+
   );
 }
 
