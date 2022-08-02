5a4f0da660834fa18f3a0173324d7b331a8c1df9
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-02 07:39:07 +0500

    Add Product List button and get data from productData reducer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,14 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addToCart, emptyCart, removeFromCart } from '../redux/action';
+import { productList } from '../redux/productAction';
 
 function Main() {
+
+  const result = useSelector((state) => state.productData);
+  console.log("Data in Main Component",result);
+
+  // const result = useSelector((state) => state);
+  // console.log("Get All Reducers Data",result);
 
   const dispatch = useDispatch();
 
@@ -11,12 +18,10 @@ function Main() {
 
     <div>
       <button onClick={() => dispatch(addToCart(product))}>Add To Cart</button>
-      <div>
-        <button onClick={() => dispatch(removeFromCart(product.name))}>Remove From Cart</button>
-        <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
-      </div>
+      <button onClick={() => dispatch(removeFromCart(product.name))}>Remove From Cart</button>
+      <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
+      <button onClick={() => dispatch(productList())}>Get Product List</button>
     </div>
-
   );
 }
 
