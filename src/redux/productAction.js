95c27ed439ee291dee2d2bca555da8d0f5d8e48d
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-04 06:52:00 +0500

    Remove API Code

--- src/redux/productAction.js
+++ src/redux/productAction.js
@@ -1,11 +1,7 @@
 import { PRODUCT_LIST } from './constant'
 
-export const productList = async () => {
-    let data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    data = await data.json();
-    console.warn("Action productList Called", data);
+export const productList = () => {
     return {
         type: PRODUCT_LIST,
-        data
     }
 }
