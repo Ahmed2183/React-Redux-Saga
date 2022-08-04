207944e8cc14cb17cc1d882171fd3d61eac286ed
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-04 06:52:30 +0500

    Remove PRODUCT_LIST case and add SET_PRODUCT_LIST case

--- src/redux/productReducer.js
+++ src/redux/productReducer.js
@@ -1,11 +1,12 @@
-import { PRODUCT_LIST } from './constant'
+import { PRODUCT_LIST, SET_PRODUCT_LIST } from './constant'
 
 export const productData = (data = [], action) => { //data = [] is initial value
   
     switch (action.type) {
-        case PRODUCT_LIST:
+       
+        case SET_PRODUCT_LIST:
             console.warn("PRODUCT_LIST CONDITION", action);
-            return [action.data];
+            return [...action.data];  // ... means eik array mai kafi array hai is lia directly extract kia
 
         default:  //When No Case Matched
             return data;
