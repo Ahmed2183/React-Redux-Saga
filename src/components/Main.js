4b66eb920481864e0c5728b403d29c2a80b5d839
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-04 07:54:42 +0500

    Show Product on UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,47 @@
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToCart, emptyCart, removeFromCart } from '../redux/action';
 import { productList } from '../redux/productAction';
 
 function Main() {
 
-  const result = useSelector((state) => state.productData);
-  console.log("Data in Main Component From Saga",result);
-
-  // const result = useSelector((state) => state);
-  // console.log("Get All Reducers Data",result);
-
   const dispatch = useDispatch();
 
-  const product = { name: 'Oppo', type: 'Mobile', price: '30000', color: 'black' }
+  const data = useSelector((state) => state.productData); //-->Call productData reducer
+  console.log("Data in Main Component From Saga", data);
+
+  // const data = useSelector((state) => state); //--> Call All reducers
+  // console.log("Get All Reducers Data",data);
+
+  useEffect(() => {
+    dispatch(productList());
+  }, [])
+
 
   return (
-
-    <div>
-      <button onClick={() => dispatch(addToCart(product))}>Add To Cart</button>
-      <button onClick={() => dispatch(removeFromCart(product.name))}>Remove From Cart</button>
-      <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
-      <button onClick={() => dispatch(productList())}>Get Product List</button>
-    </div>
+    <>
+      <div>
+        <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
+      </div>
+      <div className="product-container">
+        {
+          data.map((item) =>
+            <div className='product-item' key={item.id}>
+              <img src={item.image} alt="" />
+              <div>Name: {item.name}</div>
+              <div>Color: {item.color}</div>
+              <div>Price: {item.price}</div>
+              <div>Category: {item.category}</div>
+              <div>Brand: {item.brand}</div>
+              <div>
+                <button onClick={() => dispatch(addToCart(item))}>Add To Cart</button>
+                <button onClick={() => dispatch(removeFromCart(item.id))}>Remove From Cart</button>
+              </div>
+            </div>
+          )
+        }
+      </div>
+    </>
   );
 }
 
