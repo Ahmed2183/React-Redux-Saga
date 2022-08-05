58ccec7985be597f86f789ae7fa50d46b0481741
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-05 07:21:52 +0500

    Use BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import store from './redux/store';
+
 
 // console.warn(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
