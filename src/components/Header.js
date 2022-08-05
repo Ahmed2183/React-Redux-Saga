63a3f57dfa40bfab5b3f368062136a132b04d1c2
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-05 07:22:23 +0500

    Use Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { FaOpencart } from 'react-icons/fa'
 
 const Header = () => {
 
-  const result = useSelector((state) => state.cartData);
-  console.log("Redux Data in Header",result);
+    const result = useSelector((state) => state.cartData);
+    console.log("Redux Data in Header", result);
 
     return (
         <div className='header'>
-            <div className='cart-div'>
-                <span>{result.length}</span>
-                <FaOpencart size={65}/>
-                {/* <img src='https://i.pinimg.com/originals/4f/74/89/4f7489062aa5c4c112943f402d29373c.jpg' alt='' /> */}
-            </div>
+            <Link to='/'>
+                <h1 className='logo'>E-COMMERCE</h1>
+            </Link>
+            <Link to='/cart'>
+                <div className='cart-div'>
+                    <span>{result.length}</span>
+                    <FaOpencart size={65} />
+                    {/* <img src='https://i.pinimg.com/originals/4f/74/89/4f7489062aa5c4c112943f402d29373c.jpg' alt='' /> */}
+                </div>
+            </Link>
         </div>
     )
 }
