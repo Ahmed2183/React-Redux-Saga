5858a824b2a6bb181392ba214c3ab7c0f93d0bc4
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-05 08:48:59 +0500

    Create searchProduct funtion and call search type action with yield

--- src/redux/productSaga.js
+++ src/redux/productSaga.js
@@ -1,7 +1,8 @@
 import { takeEvery, put } from 'redux-saga/effects'  //takeEvery is for get data, put is used to call data from reducer
-import { PRODUCT_LIST, SET_PRODUCT_LIST } from './constant';
+import { PRODUCT_LIST, SET_PRODUCT_LIST, SEARCH_PRODUCT } from './constant';
 
 //Generator Function, Use yeild instead of await
+
 function* getProducts()  
 {
     let data = yield fetch("http://localhost:3500/product");
@@ -10,9 +11,18 @@ function* getProducts()
     yield put({ type: SET_PRODUCT_LIST, data:data }) //-->Saga call reducer direclty using put, SET_PRODUCT_LIST is reducer type
 }
 
+function* searchProducts(data)  
+{
+    let result = yield fetch(`http://localhost:3500/product?q=${data.query}`);
+    result = yield result.json();
+    console.warn("Action productSearch Called", data);
+    yield put({ type: SET_PRODUCT_LIST, data:result }) 
+}
+
 function* productSaga()  
 {
     yield takeEvery(PRODUCT_LIST, getProducts);   //In parameters add action type and function in which we call api of that action, PRODUCT_LIST is action type
+    yield takeEvery(SEARCH_PRODUCT, searchProducts); 
 }
 
 export default productSaga;
