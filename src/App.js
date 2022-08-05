977330cea65708636a9e90929203c1aa660a11ad
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-05 07:21:38 +0500

    Use Route,Routes for Main and Cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Cart from './components/Cart';
 
 function App() {
 
   return (
     <div className="App">
-      <Header/>
-      <Main/>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
     </div>
   );
 }
